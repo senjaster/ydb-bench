fe7b5e52c7ab9a94d7c3f741cc1295dbf013aa3e
Arseny Biryukov <arsbir@yandex-team.ru>
2025-11-18 18:38:25 +0300

    Improve parameters handling in Jobs

--- job.py
+++ job.py
@@ -43,14 +43,47 @@ class Job(BaseExecutor):
         super().__init__(bid_from, bid_to, tran_count, metrics_collector, table_folder, use_single_session)
         
         # Use default script if none provided
-        self._script_template = script if script is not None else DEFAULT_SCRIPT
+        script_template = script if script is not None else DEFAULT_SCRIPT
+        
+        # Format script with table_folder in constructor
+        self._script = script_template.format(table_folder=self._table_folder)
         
         # Detect which parameters are used in the script
-        self._uses_bid = '$bid' in self._script_template
-        self._uses_tid = '$tid' in self._script_template
-        self._uses_aid = '$aid' in self._script_template
-        self._uses_delta = '$delta' in self._script_template
-        self._uses_iteration = '$iteration' in self._script_template
+        self._uses_bid = '$bid' in self._script
+        self._uses_tid = '$tid' in self._script
+        self._uses_aid = '$aid' in self._script
+        self._uses_delta = '$delta' in self._script
+        self._uses_iteration = '$iteration' in self._script
+
+    def _build_parameters(self, iteration: int) -> dict:
+        """
+        Build parameters dictionary based on what's used in the script.
+        Generates random values for bid, tid, aid, and delta.
+        
+        Args:
+            iteration: Current iteration number
+            
+        Returns:
+            Dictionary of parameters for the query
+        """
+        # Generate random values
+        bid = randint(self._bid_from, self._bid_to)
+        tid = (bid - 1) * TELLERS_PER_BRANCH + randint(1, TELLERS_PER_BRANCH)
+        aid = (bid - 1) * ACCOUNTS_PER_BRANCH + randint(1, ACCOUNTS_PER_BRANCH)
+        delta = randint(1, 1000)
+        
+        parameters = {}
+        if self._uses_bid:
+            parameters["$bid"] = ydb.TypedValue(bid, ydb.PrimitiveType.Int32)
+        if self._uses_tid:
+            parameters["$tid"] = ydb.TypedValue(tid, ydb.PrimitiveType.Int32)
+        if self._uses_aid:
+            parameters["$aid"] = ydb.TypedValue(aid, ydb.PrimitiveType.Int32)
+        if self._uses_delta:
+            parameters["$delta"] = ydb.TypedValue(delta, ydb.PrimitiveType.Int32)
+        if self._uses_iteration:
+            parameters["$iteration"] = ydb.TypedValue(iteration, ydb.PrimitiveType.Int32)
+        return parameters
 
     async def _execute_operation(self, session: ydb.aio.QuerySession, iteration: int):
         """
@@ -60,12 +93,6 @@ class Job(BaseExecutor):
             session: YDB query session
             iteration: Current iteration number (0-based)
         """
-        # Always generate random values
-        bid = randint(self._bid_from, self._bid_to)
-        tid = (bid - 1) * TELLERS_PER_BRANCH + randint(1, TELLERS_PER_BRANCH)
-        aid = (bid - 1) * ACCOUNTS_PER_BRANCH + randint(1, ACCOUNTS_PER_BRANCH)
-        delta = randint(1, 1000)
-
         start_time = time.time()
         success = False
         error_message = ""
@@ -73,31 +100,18 @@ class Job(BaseExecutor):
         total_cpu_time_us = 0
         
         try:
-            # Format script with table_folder
-            script = self._script_template.format(table_folder=self._table_folder)
-            
-            # Build parameters dictionary based on what's used in the script
-            parameters = {}
-            if self._uses_bid:
-                parameters["$bid"] = ydb.TypedValue(bid, ydb.PrimitiveType.Int32)
-            if self._uses_tid:
-                parameters["$tid"] = ydb.TypedValue(tid, ydb.PrimitiveType.Int32)
-            if self._uses_aid:
-                parameters["$aid"] = ydb.TypedValue(aid, ydb.PrimitiveType.Int32)
-            if self._uses_delta:
-                parameters["$delta"] = ydb.TypedValue(delta, ydb.PrimitiveType.Int32)
-            if self._uses_iteration:
-                parameters["$iteration"] = ydb.TypedValue(iteration, ydb.PrimitiveType.Int32)
+            # Build parameters dictionary with random values
+            parameters = self._build_parameters(iteration)
             
             async with session.transaction() as tx:
                 async with await tx.execute(
-                    script,
+                    self._script,
                     parameters=parameters,
                     commit_tx=True,
                     stats_mode=ydb.QueryStatsMode.BASIC,
                 ) as results:
                     async for result in results:
-                        # All resultsets should be obtained to get last_query_stats
+                        # All results should be obtained to get last_query_stats
                         pass
                     total_duration_us = tx.last_query_stats.total_duration_us
                     total_cpu_time_us = tx.last_query_stats.total_cpu_time_us
